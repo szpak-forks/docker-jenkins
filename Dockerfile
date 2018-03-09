7dc980d948cd9783c3a627373654f044de6c5133
luktom <luktom@luktom.net>
2018-03-09 19:01:47 +0100

    Configuration changes

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,8 @@ RUN apk add --update docker && rm -rf /var/cache/apk/* && addgroup -g 994 jenkin
 
 USER jenkins
 
-COPY executors.groovy /usr/share/jenkins/ref/init.groovy.d/executors.groovy
-COPY setup-users.groovy /usr/share/jenkins/ref/init.groovy.d/setup-users.groovy
-COPY simple-theme.xml /var/jenkins_home/org.codefirst.SimpleThemeDecorator.xml
-COPY locale.xml /var/jenkins_home/locale.xml
-COPY jenkins.CLI.xml /var/jenkins_home/jenkins.CLI.xml
+COPY init.groovy.d/* /usr/share/jenkins/ref/init.groovy.d/
+COPY default-config/* /var/jenkins_home/
 
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
-
-COPY config.xml /var/jenkins_home/config.xml
---
+++ init.groovy.d/setup-credentials.groovy
@@ -0,0 +1,26 @@
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import hudson.plugins.sshslaves.*
+import groovy.io.FileType
+
+domain = Domain.global()
+store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+def dir = new File("/credentials/git-keys")
+
+dir.eachFileRecurse(FileType.FILES) { file ->
+  privateKey = new BasicSSHUserPrivateKey(
+    CredentialsScope.GLOBAL,
+    file.name,
+    "git",
+    new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource(file.path),
+    "",
+    ""
+  )
+
+  store.addCredentials(domain, privateKey)
+}
