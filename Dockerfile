cb56c983ce2fb4841a00822573c66a1f64e0f701
luktom <luktom@luktom.net>
2018-03-09 20:35:41 +0100

    Configuration changes

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,8 @@ RUN apk add --update docker && rm -rf /var/cache/apk/* && addgroup -g 994 jenkin
 
 COPY init.groovy.d /usr/share/jenkins/ref/init.groovy.d/
 COPY default-config /usr/share/jenkins/ref/
-COPY jobs /var/jenkins_home/
 
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
-RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt; \
-    chown -R jenkins:jenkins /var/jenkins_home
+RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
 
 USER jenkins
--- init.groovy.d/seed-job.groovy
+++ init.groovy.d/seed-job.groovy
@@ -1,2 +1,38 @@
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.BranchSpec;
+import hudson.triggers.SCMTrigger;
+import hudson.util.Secret;
+import javaposse.jobdsl.plugin.*;
+import jenkins.model.Jenkins;
+import jenkins.model.JenkinsLocationConfiguration;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import jenkins.model.JenkinsLocationConfiguration;
+import org.jenkinsci.plugins.ghprb.GhprbGitHubAuth;
+import org.jenkinsci.plugins.ghprb.GhprbTrigger.DescriptorImpl;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl.DescriptorImpl;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.Whitelist;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.BlanketWhitelist;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition;
+
 gitRepoUrl = System.getenv("JENKINS_SEED_GIT_REPO_URL")
 gitCredentialsId = System.getevn("JENKINS_SEED_GIT_CREDENTIALS_ID")
+
+jobName = "seed";
+gitTrigger = new SCMTrigger("* * * * *");
+dslBuilder = new ExecuteDslScripts(scriptLocation = new ExecuteDslScripts.ScriptLocation(value = "false", targets="main.groovy", scriptText=""), ignoreExisting=false, removedJobAction=RemovedJobAction.DELETE, removedViewAction=RemovedViewAction.DELETE);
+
+dslProject = new hudson.model.FreeStyleProject(jenkins, jobName);
+dslProject.scm = new GitSCM(gitRepoUrl);
+dslProject.scm.branches = [new BranchSpec("*/master")];
+dslProject.addTrigger(gitTrigger);
+dslProject.createTransientActions();
+dslProject.getPublishersList().add(dslBuilder);
+
+jenkins.add(dslProject, jobName);
+
+gitTrigger.start(dslProject, true);
