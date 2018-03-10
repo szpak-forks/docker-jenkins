ae6bffa8cfcf4d74cceeefa5b4835b4c89806abd
luktom <luktom@luktom.net>
2018-03-10 14:31:15 +0100

    Configuration changes

--- jenkins-docker-group.sh
+++ jenkins-docker-group.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
+echo Running as $(whoami)
+
 DOCKER_SOCKET=/var/run/docker.sock
-DOCKER_GROUP=docker
+DOCKER_GROUP=jenkins-docker
 
 if [ -S ${DOCKER_SOCKET} ]; then
     DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
