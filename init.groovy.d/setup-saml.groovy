b0de763c42ea1cc7f86d58ef78c420fafd052bef
luktom <luktom@luktom.net>
2018-03-19 15:52:27 +0100

    Configuration changes

---
+++ init.groovy.d/setup-saml.groovy
@@ -0,0 +1,32 @@
+import jenkins.model.*
+import hudson.security.*
+import hudson.util.Secret
+import org.jenkinsci.plugins.saml.*
+
+def idpMetadataUrl = System.getenv('JENKINS_SAML_IDP_METADATA_URL') ?: ''
+
+if(idpMetadataUrl) {
+  def instance = Jenkins.getInstance()
+
+  def samlRealm = new SamlSecurityRealm(
+    new IdpMetadataConfiguration(idpMetadataUrl),
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
+    "http://schemas.xmlsoap.org/claims/Group",
+    86400,
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
+    "",
+    "",
+  null,
+    new SamlEncryptionData('/var/jenkins_home/saml.jks', Secret.fromString('password'), Secret.fromString('password'), ''),
+    "none",
+    "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+    )
+
+  instance.setSecurityRealm(samlRealm)
+  instance.save()
+
+  def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+  strategy.setAllowAnonymousRead(false)
+  instance.setAuthorizationStrategy(strategy)
+  instance.save()
+}
