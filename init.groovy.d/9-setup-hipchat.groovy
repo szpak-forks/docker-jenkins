c0e51e080dcd38d2080eaf7240f5d83fe2f32fb1
luktom <luktom@luktom.net>
2018-06-26 13:17:28 +0200

    Quick commit

---
+++ init.groovy.d/9-setup-hipchat.groovy
@@ -0,0 +1,30 @@
+import jenkins.model.*;
+import java.lang.reflect.Field;
+
+def hipchatServer = System.getenv('HIPCHAT_SERVER') ?: ''
+def hipchatToken = System.getenv('HIPCHAT_TOKEN') ?: ''
+def hipchatSendAs = System.getenv('HIPCHAT_SEND_AS') ?: 'Jenkins'
+
+if ( Jenkins.instance.pluginManager.activePlugins.find { it.shortName == "hipchat" } != null && hipchatToken != '' && hipchatServer != '') {
+  println "--> setting hipchat plugin"
+
+  def descriptor = Jenkins.instance.getDescriptorByType(jenkins.plugins.hipchat.HipChatNotifier.DescriptorImpl.class)
+
+  // no setters :-(
+  // Groovy can disregard object's pivacy anyway to directly access private
+  // fields, but we use a different technique 'reflection' this time
+  Field[] fld = descriptor.class.getDeclaredFields();
+  for(Field f:fld){
+    f.setAccessible(true);
+    switch (f.getName()) {
+      case "server"         : f.set(descriptor, "hipchat.mydomain.com")
+                            break
+      case "token"          : f.set(descriptor, "TOKEN")
+                            break
+      case "buildServerUrl" : f.set(descriptor, "/")
+                            break
+      case "sendAs"         : f.set(descriptor, "jenkinsbot")
+                            break
+    }
+  }
+}
